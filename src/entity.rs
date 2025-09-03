8796f99457672e6bfc6265c0253fde623ec6eae1
Nissyaniss <kirito042004@gmail.com>
2025-09-03 21:50:22 +0200

    clippy moment

--- src/entity.rs
+++ src/entity.rs
@@ -1,263 +1,271 @@
-use std::{fmt, io::{self, Error}};
+use std::{
+	fmt,
+	io::{self, Error},
+};
 
 use rand::Rng;
 
+#[derive(Debug)]
 pub struct Player {
-    pub name: String,
-    pub health: i32,
-    pub attack: i32,
-    pub defense: i32,
-    pub speed: i32,
+	pub name: String,
+	pub health: i32,
+	pub attack: i32,
+	pub defense: i32,
+	pub speed: i32,
 
-    pub weapon: Option<Weapon>,
+	pub weapon: Option<Weapon>,
 }
 
+#[derive(Debug)]
 pub enum Action {
-    Attack,
-    Run,
+	Attack,
+	Run,
 }
 
+#[derive(Debug)]
 pub struct Weapon {
-    pub name: String,
-    pub weapon_type: WeaponType,
-    pub attack_value: i32,
-    pub rarity: Rarity,
+	pub name: String,
+	pub weapon_type: WeaponType,
+	pub attack_value: i32,
+	pub rarity: Rarity,
 }
 
+#[derive(Debug)]
 pub enum WeaponType {
-    Sword,
-    Dagger,
-    Axe,
+	Sword,
+	Dagger,
+	Axe,
 }
 
+#[derive(Debug)]
 pub enum Rarity {
-    Common,
-    Rare,
-    Epic,
-    Legendary,
-    Mythical,
+	Common,
+	Rare,
+	Epic,
+	Legendary,
+	Mythical,
 }
 
 impl fmt::Display for WeaponType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            WeaponType::Sword => write!(f, "Sword"),
-            WeaponType::Dagger => write!(f, "Dagger"),
-            WeaponType::Axe => write!(f, "Axe"),
-        }
-    }
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Self::Sword => write!(f, "Sword"),
+			Self::Dagger => write!(f, "Dagger"),
+			Self::Axe => write!(f, "Axe"),
+		}
+	}
 }
 
 impl fmt::Display for Rarity {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Rarity::Common => write!(f, "common"),
-            Rarity::Rare => write!(f, "rare"),
-            Rarity::Epic => write!(f, "epic"),
-            Rarity::Legendary => write!(f, "legendary"),
-            Rarity::Mythical => write!(f, "mythical"),
-        }
-    }
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Self::Common => write!(f, "common"),
+			Self::Rare => write!(f, "rare"),
+			Self::Epic => write!(f, "epic"),
+			Self::Legendary => write!(f, "legendary"),
+			Self::Mythical => write!(f, "mythical"),
+		}
+	}
 }
 
 impl Weapon {
-    pub fn new(weapon_type: WeaponType) -> Weapon {
-        let prefix = vec!["Broken", "Rusty", "", "Sharp"];
+	#[must_use]
+	pub fn new(weapon_type: WeaponType) -> Self {
+		let prefix = ["Broken", "Rusty", "", "Sharp"];
 
-        let weapon_prefix = prefix[rand::rng().random_range(0..prefix.len())];
+		let weapon_prefix = prefix[rand::rng().random_range(0..prefix.len())];
 
-        let name = format!("{} {}", weapon_prefix, WeaponType::Sword);
+		let name = format!("{} {}", weapon_prefix, WeaponType::Sword);
 
-        let mut attack_value = match weapon_type {
-            WeaponType::Sword => 10,
-            WeaponType::Dagger => 5,
-            WeaponType::Axe => 15,
-        };
+		let mut attack_value = match weapon_type {
+			WeaponType::Sword => 10,
+			WeaponType::Dagger => 5,
+			WeaponType::Axe => 15,
+		};
 
-        
-        attack_value = match weapon_prefix {
-            "Broken" => attack_value / 2,
-            "Rusty" => attack_value - 2,
-            "Sharp" => attack_value + 2,
-            _ => attack_value,
-        };
+		attack_value = match weapon_prefix {
+			"Broken" => attack_value / 2,
+			"Rusty" => attack_value - 2,
+			"Sharp" => attack_value + 2,
+			_ => attack_value,
+		};
 
-        let rarity = Rarity::Common;
-        
-        Weapon {
-            name,
-            weapon_type,
-            attack_value,
-            rarity,
-        }
-    }
+		let rarity = Rarity::Common;
 
-    pub fn empty() -> Weapon {
-        Weapon {
-            name: String::from(""),
-            weapon_type: WeaponType::Sword,
-            attack_value: 0,
-            rarity: Rarity::Common,
-        }
-    }
+		Self {
+			name,
+			weapon_type,
+			attack_value,
+			rarity,
+		}
+	}
+
+	#[must_use]
+	pub const fn empty() -> Self {
+		Self {
+			name: String::new(),
+			weapon_type: WeaponType::Sword,
+			attack_value: 0,
+			rarity: Rarity::Common,
+		}
+	}
 }
 
 impl Player {
-    pub fn new(name: String) -> Player {
-        Player {
-            name,
-            health: 100,
-            attack: 10,
-            defense: 5,
-            speed: 5,
+	#[must_use]
+	pub const fn new(name: String) -> Self {
+		Self {
+			name,
+			health: 100,
+			attack: 10,
+			defense: 5,
+			speed: 5,
 
-            weapon: None,
-        }
-    }
+			weapon: None,
+		}
+	}
 
-    pub fn select_action(&self) -> Action {
-        println!("What do you want to do?");
-        println!("1. Attack");
-        println!("2. Run");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("error: unable to read user input");
+	#[must_use]
+	pub fn select_action(&self) -> Action {
+		println!("What do you want to do?");
+		println!("1. Attack");
+		println!("2. Run");
+		let mut input = String::new();
+		io::stdin()
+			.read_line(&mut input)
+			.expect("error: unable to read user input");
 
-        match input.trim() {
-            "1" => Action::Attack,
-            "2" => Action::Run,
-            _ => Action::Attack,
-        }
-    }
+		match input.trim() {
+			"2" => Action::Run,
+			_ => Action::Attack,
+		}
+	}
 
-    pub fn attack(&self, target: &mut Monster) {
-        let damage: i32;
+	pub fn attack(&self, target: &mut Monster) {
+		let damage = match self.weapon.as_ref() {
+			Some(weapon) => (self.attack + weapon.attack_value) - target.defense,
+			None => self.attack - target.defense,
+		};
 
-        match self.weapon.as_ref() {
-            Some(weapon) => {
-                damage = (self.attack + weapon.attack_value) - target.defense;
-            }
-            None => {
-                damage = self.attack - target.defense;
-            }
-        }
+		let damage: i32 = match damage {
+			x if x < 0 => 0,
+			x => x,
+		};
 
-        let damage: i32 = match damage {
-            x if x < 0 => 0,
-            x => x as i32,
-        };
+		target.health -= damage;
 
-        target.health -= damage as i32;
+		println!(
+			"{} attacks {} for {} damage",
+			self.name, target.name, damage
+		);
+	}
 
-        println!(
-            "{} attacks {} for {} damage",
-            self.name, target.name, damage
-        );
-    }
+	#[must_use]
+	pub const fn is_dead(&self) -> bool {
+		self.health <= 0
+	}
 
-    pub fn is_dead(&self) -> bool {
-        self.health <= 0
-    }
-
-    pub fn equip(&mut self, weapon: Weapon) {
-        self.weapon = Some(weapon);
-    }
+	pub fn equip(&mut self, weapon: Weapon) {
+		self.weapon = Some(weapon);
+	}
 }
 
 impl fmt::Display for Player {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "===== {} =====\nHealth: {}\nAttack: {}\nDefense: {}\nSpeed: {}\n",
-            self.name, self.health, self.attack, self.defense, self.speed
-        )
-    }
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(
+			f,
+			"===== {} =====\nHealth: {}\nAttack: {}\nDefense: {}\nSpeed: {}\n",
+			self.name, self.health, self.attack, self.defense, self.speed
+		)
+	}
 }
 
+#[derive(Debug)]
 pub struct Monster {
-    pub name: String,
-    pub level: i32,
-    pub health: i32,
-    pub attack: i32,
-    pub defense: i32,
-    pub speed: i32,
+	pub name: String,
+	pub level: i32,
+	pub health: i32,
+	pub attack: i32,
+	pub defense: i32,
+	pub speed: i32,
 }
-
+#[derive(Debug)]
 pub enum MonsterType {
-    Slime,
-    Goblin,
-    Ogre,
+	Slime,
+	Goblin,
+	Ogre,
 }
 
 impl Monster {
-    fn new(monster_type: MonsterType, level: i32) -> Monster {
-        match monster_type {
-            MonsterType::Slime => Monster {
-                name: String::from("Slime"),
-                level,
-                health: level * 10,
-                attack: level * 2,
-                defense: level,
-                speed: level,
-            },
-            MonsterType::Goblin => Monster {
-                name: String::from("Goblin"),
-                level,
-                health: level * 20,
-                attack: level * 3,
-                defense: level,
-                speed: level * 5,
-            },
-            MonsterType::Ogre => Monster {
-                name: String::from("Ogre"),
-                level,
-                health: level * 30,
-                attack: level * 4,
-                defense: level * 2,
-                speed: level,
-            },
-        }
-    }
+	#[must_use]
+	fn new(monster_type: &MonsterType, level: i32) -> Self {
+		match monster_type {
+			MonsterType::Slime => Self {
+				name: String::from("Slime"),
+				level,
+				health: level * 10,
+				attack: level * 2,
+				defense: level,
+				speed: level,
+			},
+			MonsterType::Goblin => Self {
+				name: String::from("Goblin"),
+				level,
+				health: level * 20,
+				attack: level * 3,
+				defense: level,
+				speed: level * 5,
+			},
+			MonsterType::Ogre => Self {
+				name: String::from("Ogre"),
+				level,
+				health: level * 30,
+				attack: level * 4,
+				defense: level * 2,
+				speed: level,
+			},
+		}
+	}
 
-    pub fn monster(threat: i32) -> Result<Monster, Error> {
-        match threat {
-            1 => Ok(Monster::new(MonsterType::Slime, 1)),
-            2 => Ok(Monster::new(MonsterType::Goblin, 1)),
-            3 => Ok(Monster::new(MonsterType::Ogre, 1)),
-            _ => Err(Error::new(io::ErrorKind::Other, "Monster not found")),
-        }
-    }
+	pub fn monster_based_on_threat(threat: i32) -> Result<Self, Error> {
+		match threat {
+			1 => Ok(Self::new(&MonsterType::Slime, 1)),
+			2 => Ok(Self::new(&MonsterType::Goblin, 1)),
+			3 => Ok(Self::new(&MonsterType::Ogre, 1)),
+			_ => Err(Error::other("Monster not found")),
+		}
+	}
 }
 
 impl Monster {
-    pub fn is_alive(&self) -> bool {
-        self.health > 0
-    }
+	#[must_use]
+	pub const fn is_alive(&self) -> bool {
+		self.health > 0
+	}
 
-    pub fn attack(&self, target: &mut Player) {
-        let damage = self.attack - target.defense;
+	pub fn attack(&self, target: &mut Player) {
+		let damage = self.attack - target.defense;
 
-        let damage = match damage {
-            x if x < 0 => 1,
-            x => x,
-        };
+		let damage = match damage {
+			x if x < 0 => 1,
+			x => x,
+		};
 
-        target.health -= damage;
+		target.health -= damage;
 
-        println!(
-            "{} attacks {} for {} damage",
-            self.name, target.name, damage
-        );
-    }
+		println!(
+			"{} attacks {} for {} damage",
+			self.name, target.name, damage
+		);
+	}
 }
 
 impl fmt::Display for Monster {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "===== {} =====\nHealth: {}\nAttack: {}\nDefense: {}\nSpeed: {}\n",
-            self.name, self.health, self.attack, self.defense, self.speed
-        )
-    }
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(
+			f,
+			"===== {} =====\nHealth: {}\nAttack: {}\nDefense: {}\nSpeed: {}\n",
+			self.name, self.health, self.attack, self.defense, self.speed
+		)
+	}
 }
--- src/room.rs
+++ src/room.rs
@@ -5,223 +5,250 @@ use rand::Rng;
 
 use crate::entity::{Action, Monster, Player, Weapon, WeaponType};
 
+#[derive(Debug)]
 pub struct Room {
-    name: String,
-    description: String,
-    difficulty: Difficulty,
-    monsters: Vec<Monster>,
-    treasure: Option<Treasure>,
+	name: String,
+	description: String,
+	difficulty: Difficulty,
+	monsters: Vec<Monster>,
+	treasure: Option<Treasure>,
 }
 
+#[derive(Debug)]
 enum Difficulty {
-    Easy,
-    Medium,
-    Hard,
+	Easy,
+	Medium,
+	Hard,
 }
 
 impl fmt::Display for Difficulty {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Difficulty::Easy => write!(f, "Easy"),
-            Difficulty::Medium => write!(f, "Medium"),
-            Difficulty::Hard => write!(f, "Hard"),
-        }
-    }
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Self::Easy => write!(f, "Easy"),
+			Self::Medium => write!(f, "Medium"),
+			Self::Hard => write!(f, "Hard"),
+		}
+	}
 }
 
+#[derive(Debug)]
 pub enum RoomResult {
-    Sucess,
-    Died,
-    Ran,
+	Sucess,
+	Died,
+	Ran,
 }
 
 impl Room {
-    pub fn display(&self) {
-        println!("============================");
-        println!("📍 Room : {}", self.name);
-        println!("============================\n");
-        println!("{}", self.description);
-        println!("\n🔹 Difficulty : {}\n", self.difficulty);
+	pub fn display(&self) {
+		println!("============================");
+		println!("📍 Room : {}", self.name);
+		println!("============================\n");
+		println!("{}", self.description);
+		println!("\n🔹 Difficulty : {}\n", self.difficulty);
 
-        println!("👹 Monsters :");
-        if self.monsters.is_empty() {
-            println!("  - No monsters here.");
-        } else {
-            for monster in &self.monsters {
-                println!("  - {} (Level {})", monster.name, monster.level);
-            }
-        }
+		println!("👹 Monsters :");
+		if self.monsters.is_empty() {
+			println!("  - No monsters here.");
+		} else {
+			for monster in &self.monsters {
+				println!("  - {} (Level {})", monster.name, monster.level);
+			}
+		}
 
-        println!("\n💰 Treasure :");
-        match &self.treasure {
-            Some(t) => {
-                match &t.weapon {
-                    Some(w) => println!("  - A {} weapon", w.rarity),
-                    None => println!("")
-                }
-                println!("  - Treasure: {} golds", t.gold)
-            },
-            None => println!("  - Aucun trésor ici.")
-        }
+		println!("\n💰 Treasure :");
+		match &self.treasure {
+			Some(t) => {
+				match &t.weapon {
+					Some(w) => println!("  - A {} weapon", w.rarity),
+					None => println!(""),
+				}
+				println!("  - Treasure: {} golds", t.gold);
+			}
+			None => println!("  - Aucun trésor ici."),
+		}
 
-        println!("\n============================\n");
-    }
+		println!("\n============================\n");
+	}
 
-    pub fn new(name: String, description: String, threat: i32) -> Room {
-        let mut actual_threat = threat;
-        let mut monsters: Vec<Monster> = Vec::new();
+	#[must_use]
+	pub fn new(name: String, description: String, threat: i32) -> Self {
+		let mut actual_threat = threat;
+		let mut monsters: Vec<Monster> = Vec::new();
 
-        while actual_threat > 0 {
-            let num = rand::rng().random_range(1..(if actual_threat > 3 { actual_threat } else { actual_threat + 2 }));
-            let monster = Monster::monster(num);
+		while actual_threat > 0 {
+			let num = rand::rng().random_range(
+				1..(if actual_threat > 3 {
+					actual_threat
+				} else {
+					actual_threat + 2
+				}),
+			);
+			let monster = Monster::monster_based_on_threat(num);
 
-            match monster {
-                Ok(m) => {
-                    monsters.push(m);
-                    actual_threat -= num
-                },
-                Err(_) => continue,
-            }
-        }
+			if let Ok(m) = monster {
+				monsters.push(m);
+				actual_threat -= num;
+			}
+		}
 
-        let difficulty = match actual_threat {
-            x if x < 0 => Difficulty::Medium,
-            x if x < -3 => Difficulty::Hard,
-            _ => Difficulty::Easy,
-        };
+		let difficulty = match actual_threat {
+			x if x < 0 => Difficulty::Medium,
+			x if x < -3 => Difficulty::Hard,
+			_ => Difficulty::Easy,
+		};
 
-        Room {
-            name,
-            description,
-            difficulty,
-            monsters,
-            treasure: Some(Treasure::new()),
-        }
-    }
+		Self {
+			name,
+			description,
+			difficulty,
+			monsters,
+			treasure: Some(Treasure::new()),
+		}
+	}
 
-    pub fn enter(&mut self, player: &mut Player) -> RoomResult {
-        println!("You enter a room.\n");
+	pub fn enter(&mut self, player: &mut Player) -> RoomResult {
+		println!("You enter a room.\n");
 
-        self.display();
+		self.display();
 
-        println!("Continue...");
+		println!("Continue...");
 
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("error: unable to read user input");
+		let mut input = String::new();
+		io::stdin()
+			.read_line(&mut input)
+			.expect("error: unable to read user input");
 
-        print!("\x1bc\x1b[1;1H"); // clear screen
+		print!("\x1bc\x1b[1;1H"); // clear screen
 
-        for monster in self.monsters.iter_mut() {
-            println!("A level {} {} appears!\n", monster.level, monster.name);
+		for monster in self.monsters.iter_mut() {
+			println!("A level {} {} appears!\n", monster.level, monster.name);
 
-            while monster.is_alive() {
-                display_fight_screen(player, monster);
+			while monster.is_alive() {
+				display_fight_screen(player, monster);
 
-                let action = player.select_action();
+				let action = player.select_action();
 
-                print!("\x1bc\x1b[1;1H"); // clear screen
-                if player.speed > monster.speed {
+				print!("\x1bc\x1b[1;1H"); // clear screen
+				if player.speed > monster.speed {
+					match action {
+						Action::Attack => {
+							player.attack(monster);
+						}
+						Action::Run => return RoomResult::Ran,
+					}
 
-                    match action {
-                        Action::Attack => {
-                            player.attack(monster);
-                        }
-                        Action::Run => return RoomResult::Ran,
-                    }
+					if monster.is_alive() {
+						monster.attack(player);
+					}
+				} else {
+					monster.attack(player);
 
-                    if monster.is_alive() {
-                        monster.attack(player);
-                    }
-                } else {
-                    monster.attack(player);
+					match action {
+						Action::Attack => {
+							player.attack(monster);
+						}
+						Action::Run => return RoomResult::Ran,
+					}
+				}
 
-                    match action {
-                        Action::Attack => {
-                            player.attack(monster);
-                        }
-                        Action::Run => return RoomResult::Ran,
-                    }
-                }
+				if player.is_dead() {
+					return RoomResult::Died;
+				}
+			}
 
-                if player.is_dead() {
-                    return RoomResult::Died;
-                }
-            }
+			println!("You killed the {}!", monster.name);
+		}
 
-            println!("You killed the {}!", monster.name);
-        }
+		if let Some(treasure) = self.treasure.take() {
+			if let Some(weapon) = treasure.weapon {
+				println!("You found a weapon!"); //, treasure.weapon.as_ref().unwrap().name);
+				println!(
+					"Your attack: {} -> New attack: {}",
+					player.attack
+						+ player
+							.weapon
+							.as_ref()
+							.unwrap_or(&Weapon::empty())
+							.attack_value,
+					player.attack + weapon.attack_value
+				);
+				println!("Do you want to equip it? (y/N)");
 
-        if let Some(treasure) = self.treasure.take() {
-            if let Some(weapon) = treasure.weapon {
-                println!("You found a weapon!"); //, treasure.weapon.as_ref().unwrap().name);
-                println!("Your attack: {} -> New attack: {}", player.attack + player.weapon.as_ref().unwrap_or(&Weapon::empty()).attack_value, player.attack + weapon.attack_value);
-                println!("Do you want to equip it? (y/N)");
+				let mut input = String::new();
+				io::stdin()
+					.read_line(&mut input)
+					.expect("error: unable to read user input");
 
-                let mut input = String::new();
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("error: unable to read user input");
+				match input.trim() {
+					"y" => player.equip(weapon),
+					_ => println!("You decide not to equip the weapon."),
+				}
+			}
 
-                match input.trim() {
-                    "y" => player.equip(weapon),
-                    _ => println!("You decide not to equip the weapon."),
-                }
-            }
+			if let Some(health_potion) = treasure.health_potion {
+				println!("You found a health potion!");
+				println!(
+					"Your health: {} -> New health: {}",
+					player.health,
+					player.health + health_potion.heal_amount
+				);
+				player.health += health_potion.heal_amount;
+			}
+		}
 
-            if let Some(health_potion) = treasure.health_potion {
-                println!("You found a health potion!");
-                println!("Your health: {} -> New health: {}", player.health, player.health + health_potion.heal_amount);
-                player.health += health_potion.heal_amount;
-            }
-        }
-
-        RoomResult::Sucess
-    }
+		RoomResult::Sucess
+	}
 }
 
+#[derive(Debug)]
 struct Treasure {
-    gold: i32,
-    weapon: Option<Weapon>,
-    health_potion: Option<HealthPotion>,
+	gold: i32,
+	weapon: Option<Weapon>,
+	health_potion: Option<HealthPotion>,
 }
 
 impl Treasure {
-    fn new() -> Treasure {
-        Treasure {
-            gold: rand::rng().random_range(10..50),
-            weapon: Some(Weapon::new(WeaponType::Sword)),
-            // armor: None,
-            health_potion: None,
-        }
-    }
+	fn new() -> Self {
+		Self {
+			gold: rand::rng().random_range(10..50),
+			weapon: Some(Weapon::new(WeaponType::Sword)),
+			// armor: None,
+			health_potion: None,
+		}
+	}
 }
 
+#[derive(Debug)]
 struct HealthPotion {
-    heal_amount: i32,
+	heal_amount: i32,
 }
 
 fn display_fight_screen(player: &Player, monster: &Monster) {
-    println!(" ==============================================");
-    println!("  *--*-              BATTLE              *-*--* ");
-    println!(" ==============================================\n");
+	println!(" ==============================================");
+	println!("  *--*-              BATTLE              *-*--* ");
+	println!(" ==============================================\n");
 
-    println!(
-        "===== {} =====\t\t\t===== {} =====",
-        player.name, monster.name
-    );
-    println!(
-        " Health: {}\t\t\t Health: {}",
-        player.health, monster.health
-    );
-    println!(
-        " Attack: {}\t\t\t Attack: {}",
-        player.attack + player.weapon.as_ref().unwrap_or(&Weapon::empty()).attack_value, monster.attack
-    );
-    println!(
-        " Defense: {}\t\t\t Defense: {}",
-        player.defense, monster.defense
-    );
-    println!(" Speed: {}\t\t\t Speed: {}\n", player.speed, monster.speed);
+	println!(
+		"===== {} =====\t\t\t===== {} =====",
+		player.name, monster.name
+	);
+	println!(
+		" Health: {}\t\t\t Health: {}",
+		player.health, monster.health
+	);
+	println!(
+		" Attack: {}\t\t\t Attack: {}",
+		player.attack
+			+ player
+				.weapon
+				.as_ref()
+				.unwrap_or(&Weapon::empty())
+				.attack_value,
+		monster.attack
+	);
+	println!(
+		" Defense: {}\t\t\t Defense: {}",
+		player.defense, monster.defense
+	);
+	println!(" Speed: {}\t\t\t Speed: {}\n", player.speed, monster.speed);
 }
